2a6dd4cf2068f1edad2759f4096121aed413c9f3
Malou Øvlesen <malouoevlesen@gmail.com>
2019-05-27 11:35:35 +0200

    Updated.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,13 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
-      res.render('home');
+      res.render('home', {'title': 'Home - Skullcandy'}); //Dette indlæser ejs filen
    });
 
    app.get('/product', (req, res, next) => {
-      res.render('product');
+      res.render('product', {'title': 'Product - Skullcandy'});
    });
-
-   app.get('/contact', (req, res, next) => {
-      res.render('contact');
-   });
-
    
+   app.get('/contact', (req, res, next) => {
+      res.render('contact', {'title': 'Contact - Skullcandy'});
+   });
 };
